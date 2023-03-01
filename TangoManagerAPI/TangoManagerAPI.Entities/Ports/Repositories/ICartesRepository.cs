2cf0ac486e3d6f65dd3fc22515dce76f59caf684
Swippee <123509408+Swippee@users.noreply.github.com>
2023-03-01 13:47:16 +0100

    CarteRepo interface

---
+++ TangoManagerAPI/TangoManagerAPI.Entities/Ports/Repositories/ICartesRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TangoManagerAPI.Entities.Models;
+
+namespace TangoManagerAPI.Entities.Ports.Repositories;
+
+public interface ICartesRepository
+{
+    Task AddCarteAsync(CarteEntity Carte);
+  
+    Task<IEnumerable<CarteEntity>> GetCartesAsync();
+    Task<IEnumerable<CarteEntity>> GetCartesByPaquetNameAsync(string paquetName);
+}
